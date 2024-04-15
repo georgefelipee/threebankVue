2ce09460f83544136e8e04e09900e1428b16c407
George Felipe <georgefelipe938@gmail.com>
2024-04-14 21:20:59 -0300

    login

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+        <div>
+            img
+        </div>
+
+    </main>
+</template>
+<script>
+export default {
+    name: 'Login',
+}
+</script>
+<style lang="ts">
+    
+</style>
